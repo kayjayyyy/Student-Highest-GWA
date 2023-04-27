42beecc9caac0b8b8f576b0833b60a4ca8eddf3e
unknown <templanzatintin@gmail.com>
2023-04-27 22:59:56 +0800

    Intro/Greetings

--- StudentGWA.py
+++ StudentGWA.py
@@ -1,3 +1,9 @@
 # Templanza, Kristine Joy F.
 # BSCPE 1-4
 # Asignnment no. 3 - Student with Highest GWA
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 35)
+print("")
+print("\033[45m ♥ Welcome to our program! ♥ \033[0m".center(78))
