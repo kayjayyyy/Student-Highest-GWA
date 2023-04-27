01b90199859893a28022ce5559a149d369acb508
unknown <templanzatintin@gmail.com>
2023-04-28 00:39:21 +0800

    Assign variable

--- StudentGWA.py
+++ StudentGWA.py
@@ -12,3 +12,7 @@ print("\033[45m ♥ Welcome to our program! ♥ \033[0m".center(78))
 name = input("\n\033[033mGood day! What is your name? \033[0m")
 print("\n\033[3;33mI hope you are doing well,", name + "!\033[0m")
 print("")
+
+# Assign for the GWA value and highest student name
+lowest_GWA = 5.0
+name_of_the_highest_student = ""
